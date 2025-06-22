f12c6639c29f1c9b025376c0276b9b01a090790b
José Edson Carneiro Agra Júnior <102812154+junior25dunedain@users.noreply.github.com>
2025-06-22 18:22:50 -0300

    Add files via upload

---
+++ store/schemas/product.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from decimal import Decimal
+from typing import Annotated, Optional
+from pydantic import UUID4, Field, BaseModel, model_validator
+from bson import Decimal128
+
+from store.schemas.base import BaseSchemaMixin, OutMixin
+
+class ProductBase(BaseModel):
+    name: str = Field(...,description='Product name')
+    quantity: int = Field(...,description='Product quantity')
+    price: Decimal = Field(...,description='Product price')
+    status: bool = Field(...,description='Product status')
+
+class ProductIn(ProductBase,BaseSchemaMixin):
+    ...
+
+class ProductOut(ProductIn,OutMixin):
+    ...
+
+
+
+def convert_decimal_128(v):
+    return Decimal128(str(v))
+
+Decimal_ = Annotated[Decimal,AfterValidator(convert_decimal_128)]
+
+class ProductUpdate(BaseSchemaMixin):
+    quantity: Optional[int] = Field(None,description='Product quantity')
+    price: Optional[Decimal_] = Field(None,description='Product price')
+    status: Optional[bool] = Field(None,description='Product status')
+
+
+
+class ProductUpdateOut(ProductOut):
+    ...
